0bbf2e429588d738ee43aed318413a44db5dd174
Khodov <prophet322@mail.ru>
2024-11-30 14:32:43 +0300

    settings.py

--- mygameproject/settings.py
+++ mygameproject/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task1',
-    #'blog'
+    #'task1',
+     'blog'
 ]
 
 MIDDLEWARE = [
